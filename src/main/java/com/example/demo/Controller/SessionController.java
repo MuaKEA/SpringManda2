d68585025d1000d88d3a81317700bc1b9fd59810
Jaqoub <33924194+Jaqoub@users.noreply.github.com>
2018-11-20 15:30:04 +0100

    fkfk

--- src/main/java/com/example/demo/Controller/SessionController.java
+++ src/main/java/com/example/demo/Controller/SessionController.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 @Controller
 public class SessionController {
@@ -23,4 +26,23 @@ public class SessionController {
         model.addAttribute("loginFailed", loginFailed);
         return "/login";
     }
+
+   /* @PostMapping("/login")
+    public String login(
+        @RequestParam("username") String userName,
+        @RequestParam("passWord") String passWord,
+        @RequestParam("action") String action,
+        HttpSession session) throws SQLException{
+
+    if(action.equals("Log Ind"))
+        if(!Authenticate.login(userName, passWord, session)){
+            loginFailed = true;
+        }else{
+        loginFailed = false;
+        return (int) session.getAttribute("NIVEAU") == 2 ? "redirect:/reservation/create" : (int) session.getAttribute("NIVEAU") == 1 ? "redirect:/login");
+        }
+    }
+    }
+    */
+
 }
